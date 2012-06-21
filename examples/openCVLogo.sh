f1048394c85b4288814a02f5a44199cbbb9ccd4d
Bradlee Speice <bspeice@uncc.edu>
2012-06-21 15:56:38 -0400

    Add an example usage for the OpenCV logo

---
+++ examples/openCVLogo.sh
@@ -0,0 +1,36 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: openCVLogo.sh
+# 
+#         USAGE: ./openCVLogo.sh 
+# 
+#   DESCRIPTION: This is a test script to demonstrate libcvautomation
+#					We are going to find different sections of the OpenCV logo
+#					in itself.
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Bradlee Speice (), bspeice.nc@gmail.com
+#  ORGANIZATION: 
+#       CREATED: 06/21/2012 03:45:19 PM EDT
+#      REVISION:  ---
+#===============================================================================
+
+CVAMATCH="`dirname $0`/../bin/cva-match"
+
+if [ ! -x $CVAMATCH ]; then
+	echo "The cva-match binary was not found or is not executable. Please fix this!"
+	exit 255
+fi
+
+echo
+echo "Finding the red circle in the OpenCV logo..."
+echo "The red circle is located at: `$CVAMATCH -r opencvlogo.png -s redcircle.png -p ', ' | cut -d',' -f2-`"
+echo "Finding the blue circle in the OpenCV logo..."
+echo "The blue circle is located at: `$CVAMATCH -r opencvlogo.png -s bluecircle.png -p ', ' | cut -d',' -f2-`"
+echo "Finding the green circle in the OpenCV logo..."
+echo "The green circle is located at: `$CVAMATCH -r opencvlogo.png -s greencircle.png -p ', ' | cut -d',' -f2-`"
+echo
