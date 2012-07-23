6bc9459dfe1f508f97136ead3bbefdcdda7097d5
Bradlee Speice <bspeice@uncc.edu>
2012-07-23 10:27:51 -0400

    Continue debian/ cleanup

--- debian/libcvautomation.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the libcvautomation package
-#
-0 4	* * *	root	[ -x /usr/bin/libcvautomation_maintenance ] && /usr/bin/libcvautomation_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(libcvautomation):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="libcvautomation" command="/usr/bin/libcvautomation"
