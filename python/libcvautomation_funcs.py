d7de401d661c8d862ea30a8948454ab65a49590e
Bradlee Speice <bspeice@uncc.edu>
2012-08-14 13:35:16 -0400

    Add initial libcvautomation_funcs.py and documentation

---
+++ python/libcvautomation_funcs.py
@@ -0,0 +1,4 @@
+## \file libcvautomation_funcs.py
+# \brief Libcvautomation wrapper for python
+# \details This source file is designed to give high-level access to libcvautomation using Python. It was modeled after the Bash wrapper, but allows for access to all the extra features of Python.
+# \author Bradlee Speice
